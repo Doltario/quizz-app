aa7936578b4b9045b4736e9b3f4c070bd05ba434
Félix Darraud <felix.darraud@gmail.com>
2020-04-03 15:50:06 +0200

    Improved visual

--- src/components/quizzApp/QuizzApp.js
+++ src/components/quizzApp/QuizzApp.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './quizzApp.css'
 
+const currentTabMap = {
+  LESSONS: 'lessons',
+  CARDS: 'cards'
+}
+
 class QuizzApp extends React.Component {
   render() {
     const { currentTab } = this.props
 
     return (
-      <span id="quizz-app-container">
-        <span className="quizz-app">{currentTab}</span>
-      </span>
+      <div id="quizz-app-container">
+        <div className="quizz-app-header">
+          <span className={`quizz-app-header-item ${currentTab === currentTabMap.LESSONS ? 'quizz-app-header-item-active' : ''}`}>Lessons</span>
+          <span className={`quizz-app-header-item ${currentTab === currentTabMap.CARDS ? 'quizz-app-header-item-active' : ''}`}>Cards (@TODO: cards number)</span>
+        </div>
+        <div className="quizz-app-body">Lessons or cards will go here</div>
+      </div>
     )
   }
 }
